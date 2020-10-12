29ba3e9dfbff1810055a749c3283fa9cab277c01
Felix Benning <felix.benning@gmail.com>
2020-10-13 00:19:48 +0200

    skeleton

--- OrderStatistics/src/OrderStatistics.jl
+++ OrderStatistics/src/OrderStatistics.jl
@@ -2,12 +2,23 @@
 module OrderStatistics
 
 include("rngSquares.jl")
-import Test: @test
-import rngSquares
+import CUDA
 
-function helloWorld()
-    rngSquares.squares_rng(0, rngSquares.key)
+function gpu_parallel!(results, callable, sampleSize)
+    index = (CUDA.blockIdx().x - 1) * CUDA.blockDim().x + CUDA.threadIdx().x
+    stride = CUDA.blockDim().x * gridDim().x
+    for thread  = index:stride:length(results)
+        results[thread] = callable(thread)
+    end
+end
 
+function sample_extreme_values(sampleSize, superSampleSize, pseudoInverse)::T where T
+    numblocks = ceil(Int, sampleSize/256)
+    gpu_res = CUDA.CuArray{T}(undef, superSampleSize)
+    cpu_res = Array{T}(undef, superSampleSize)
+    @cuda threads=256 blocks=numblocks gpu_parallel!(gpu_res, pseudoInverse, sampleSize)
+    copyto!(cpu_res, gpu_res)
+    return cpu_res
 end
 
 end # module
