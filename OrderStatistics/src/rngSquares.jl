6a476946310c160738ab4be737a3268ec3053504
Felix Benning <felix.benning@gmail.com>
2020-10-16 16:01:22 +0200

    const as function

--- OrderStatistics/src/rngSquares.jl
+++ OrderStatistics/src/rngSquares.jl
@@ -13,10 +13,12 @@ export squares_rng
 # key (seed) taken from keys.h (line 2193) in the distribution of Squares
 # see https://squaresrng.wixsite.com/rand
 
-# this distribution also includes a generator for these keys - eventually
-# this hardcoded key should be replaced with such a generator 
-# (one key generates 2^64 random numbers)
-key = 0x86d47f132b79acfd
+@inline function get_seed()::UInt64
+    # this distribution also includes a generator for these keys - eventually
+    # this hardcoded key should be replaced with such a generator 
+    # (one key generates 2^64 random numbers)
+    return 0x86d47f132b79acfd
+end
 
 @inline function squares_rng(counter::UInt64, seed::UInt64)::UInt32
     yy = counter * seed
@@ -42,14 +44,14 @@ end
 
 function Base.iterate(rvg::RVGenerator, state::UInt64=UInt64(0))
     if rvg.stop >= state
-        return (rvg.pseudoInverse(Float32(squares_rng(state, key))/typemax(UInt32)), state+1)
+        return (rvg.pseudoInverse(Float32(squares_rng(state, get_seed()))/typemax(UInt32)), state+1)
     else
         return nothing
     end
 end
 
 @testset "Staticial Sanity Check" begin
-    rv = uniform(UInt32(10^8), key)
+    rv = uniform(UInt32(10^8), get_seed())
     @test isapprox(mean(rv), 0.5)
     @test isapprox(var(rv), 1/12)
 end
