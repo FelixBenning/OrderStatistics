3df8629d5c4dcfad9cd61b8e836a7cb8cf1fcd6e
Felix Benning <felix.benning@gmail.com>
2020-10-16 16:09:01 +0200

    return to functional programming

--- OrderStatistics/src/OrderStatistics.jl
+++ OrderStatistics/src/OrderStatistics.jl
@@ -4,38 +4,14 @@ module OrderStatistics
 import CUDA
 import CUDA: @cuda
 
-# Implementation of Widynski, Bernard (2020). 
-# "Squares: A Fast Counter-Based RNG". arXiv:2004.06278v2
-
-# key (seed) taken from keys.h (line 2193) in the distribution of Squares
-# see https://squaresrng.wixsite.com/rand
-
-# this distribution also includes a generator for these keys - eventually
-# this hardcoded key should be replaced with such a generator 
-# (one key generates 2^64 random numbers)
-@inline function squares_rng(counter::UInt64)::UInt32
-    seed::UInt64 = 0x86d47f132b79acfd
-    yy = counter * seed
-    z = yy + seed
-    xx = yy * (yy+1)
-    # >> arithmetic rightshift, >>> logical rightshift 
-    # (most C Impl.: >> arithm on signed, logical on unsigned)
-    # << logical/arithmetic leftshift
-    xx = (xx >>> 32) | (xx << 32) 
-    xx = xx*xx + z
-    xx = (xx >>> 32) | (xx << 32)
-    return UInt32((xx*xx + yy) >> 32)
-end
+include("rngSquares.jl")
 
 function gpu_parallel!(results, sampleSize, pseudoInverse)
     index = (CUDA.blockIdx().x - 1) * CUDA.blockDim().x + CUDA.threadIdx().x
     stride = CUDA.blockDim().x * CUDA.gridDim().x
     for thread in index:stride:length(results)
-        result = 0
-        for idx in 1:sampleSize
-            result = max(result, pseudoInverse(Float32(squares_rng(UInt64(idx)))/typemax(UInt32)))
-        end
-        results[thread] = result 
+        rvg = RVGenerator(pseudoInverse, UInt64(sampleSize))
+        results[thread] = largest(rvg)
     end
 end
 
@@ -48,7 +24,7 @@ function sample_extreme_values(sampleSize, superSampleSize, pseudoInverse)::Arra
     return cpu_res
 end
 
-function largest(generator)
+function largest(generator::RVGenerator{T}) where T
     largest = Float32(0)
     for rv = generator
         largest = max(largest, rv)
