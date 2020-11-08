42dea7ced8280e89d4a9ba83d08099ef2b83fe82
Felix Benning <felix.benning@gmail.com>
2020-11-08 17:39:06 +0100

    box muller

--- OrderStatistics/src/OrderStatistics.jl
+++ OrderStatistics/src/OrderStatistics.jl
@@ -6,32 +6,32 @@ import CUDA: @cuda
 
 include("rngSquares.jl")
 
-function gpu_parallel!(results, sampleSize, pseudoInverse, seed::UInt64, callable)
+function gpu_parallel!(results, sampleSize, pseudoInverse, seed::UInt64, callable, rng::RNG)
     index = (CUDA.blockIdx().x - 1) * CUDA.blockDim().x + CUDA.threadIdx().x
     stride = CUDA.blockDim().x * CUDA.gridDim().x
     for thread in index:stride:length(results)
-        rvg = RVGenerator(pseudoInverse, UInt64(thread), UInt64(length(results)), UInt64(sampleSize), seed)
+        rvg = rng(pseudoInverse, UInt64(thread), UInt64(length(results)), UInt64(sampleSize), seed)
         results[thread] = callable(rvg)
     end
 end
 
-function sample_extreme_values(sampleSize, superSampleSize, pseudoInverse, callable=largest, seed=get_seed())::Array{Float32,1}
+function sample_extreme_values(sampleSize, superSampleSize, pseudoInverse; callable=largest, seed=get_seed(), rng::RNG=unifWithPseudoInvGen)::Array{Float32,1}
     if typeof(pseudoInverse(0.5)) <: Tuple
-        # monte carlo pseudoInverse (can fail -> Tuple{rv, success::Bool})
+        # monte carlo pseudoInverse (can fail -> Tuple{rv, accepted::Bool})
         fixedPseudoInverse = pseudoInverse
     else
-        # normal pseudoInverse (can not fail -> always return success=true)
+        # normal pseudoInverse (can not fail -> always return accepted=true)
         fixedPseudoInverse = (x -> (pseudoInverse(x), true))
     end
     numblocks = ceil(Int, superSampleSize/256)
     gpu_res = CUDA.CuArray{Float32}(undef, superSampleSize)
     cpu_res = Array{Float32}(undef, superSampleSize)
-    @cuda threads=256 blocks=numblocks gpu_parallel!(gpu_res, sampleSize, fixedPseudoInverse, seed, callable)
+    @cuda threads=256 blocks=numblocks gpu_parallel!(gpu_res, sampleSize, fixedPseudoInverse, seed, callable, rng)
     copyto!(cpu_res, gpu_res)
     return cpu_res
 end
 
-function largest(generator::RVGenerator{T}) where T
+function largest(generator::unifWithPseudoInvGen{T}) where T
     largest = Float32(0)
     for rv = generator
         largest = max(largest, rv)
@@ -39,7 +39,7 @@ function largest(generator::RVGenerator{T}) where T
     return largest
 end
 
-function diff_largest_second(generator::RVGenerator{T}) where T
+function diff_largest_second(generator::unifWithPseudoInvGen{T}) where T
     largest = Float32(0)
     second = Float32(0)
     for rv in generator
--- OrderStatistics/src/rngSquares.jl
+++ OrderStatistics/src/rngSquares.jl
@@ -37,7 +37,9 @@ end
     return [Float32(squares_rng(UInt64(ctr), seed))/typemax(UInt32) for ctr in 1:number]
 end
 
-struct RVGenerator{T}
+abstract type RNG end
+
+struct unifWithPseudoInvGen{T} <: RNG
     pseudoInverse::T
     start::UInt64
     stride::UInt64
@@ -45,15 +47,49 @@ struct RVGenerator{T}
     seed::UInt64
 end
 
-function Base.iterate(rvg::RVGenerator, state::Tuple{UInt64,UInt64}=(UInt64(0),UInt64(0)))
+function Base.iterate(rvg::unifWithPseudoInvGen, state::Tuple{UInt64,UInt64}=(UInt64(0),UInt64(0)))
     if rvg.stop >= state[1]
         attempt = state[2]
         while true
-            rv, success = rvg.pseudoInverse(Float32(squares_rng(rvg.start + rvg.stride * attempt, rvg.seed))/typemax(UInt32))
+            rv, accepted = rvg.pseudoInverse(Float32(squares_rng(rvg.start + rvg.stride * attempt, rvg.seed))/typemax(UInt32))
             attempt += 1 # monte carlo attempts
-            if success
+            if accepted
                 return rv, (state[1]+1,attempt)
             end
+        end
+    else
+        return nothing
+    end
+end
+
+struct bufferedNormWithTransformation{T} <: RNG
+    pseudoInverse::T
+    start::UInt64
+    stride::UInt64
+    stop::UInt64
+    seed::UInt64
+end
+
+function box_muller(x)
+    scalar = sqrt(-2*log(x[1]))
+    return (scalar*cos(2*pi*x[2]), scalar*sin(2*pi*x[2]))
+end
+
+function Base.iterate(rng::bufferedNormWithTransformation, state=(UInt64(0), Float32(0.0)))
+    if rng.stop >= state[1]
+        if state % 2 # buffer full
+            return state[2]
+        else # fill buffer and return first elements
+            buffer = [
+                Float32(squares_rng(
+                    rng.start + rng.stride * (state[1] + UInt64(idx)), 
+                    rng.seed)
+                )/typemax(UInt32) 
+                for idx in 0:1
+            ]
+
+            rv, buffer = rng.pseudoInverse.(box_muller(buffer))
+            return rv, [state+1, buffer]
         end
     else
         return nothing
