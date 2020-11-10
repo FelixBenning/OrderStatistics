15157c4abe1f85e6e45a9ffd4ff60f8a1de6af0e
Felix Benning <felix.benning@gmail.com>
2020-11-10 22:46:33 +0100

    working notebook - missing normal dist

--- OrderStatistics/notebooks/notebook.jl
+++ OrderStatistics/notebooks/notebook.jl
@@ -5,11 +5,61 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ 98aec1c2-21e1-11eb-31dd-a19c407f373c
-using OrderStatistics:sample_extreme_values, diff_largest_second
+begin
+	using OrderStatistics:sample_extreme_values, diff_largest_second
+	using Statistics:mean,std
+	using Plots:plot, plot!
+	using LaTeXStrings
+end
 
 # ╔═╡ dca3e9a0-21e1-11eb-22b4-07c891533980
-sample_extreme_values(10^6, 10^6, x->x; callable=diff_largest_second)
+sample_extreme_values(10^6, 10^4, x->x, diff_largest_second)
+
+# ╔═╡ 32c4fca2-2392-11eb-133e-213db77f04d1
+times = [10, 10^2, 10^3, 5*10^3, 10^4, 5*10^4, 10^5, 10^6]
+
+# ╔═╡ bcc952c0-2392-11eb-1e54-0b4d8fa5c73b
+begin
+	unif_mean = [
+		mean(sample_extreme_values(t, 10^4, x->x, diff_largest_second))
+		for t in times
+	]
+	exp_mean = [
+		mean(sample_extreme_values(t, 10^4, x->-log(x), diff_largest_second))
+		for t in times
+	]
+	exp_mean_2 = [
+		mean(sample_extreme_values(t, 10^4, x->-log(x)/2, diff_largest_second))
+		for t in times
+	]
+	exp_mean_05 = [
+		mean(sample_extreme_values(t, 10^4, x->-log(x)*2, diff_largest_second))
+		for t in times
+	]
+	heavy_tailed_mean = [
+		mean(sample_extreme_values(t, 10^4, x->1/sqrt(x), diff_largest_second))
+		for t in times
+	]
+end
+
+# ╔═╡ d1d3f3c0-2395-11eb-00ea-59aa6de8c60f
+begin
+	p = plot(xaxis=:log, yaxis=:log)
+	plot!(p, times, unif_mean, label ="U[0,1]", markershape=:auto)
+	plot!(p, times, exp_mean, label = "Exp(1)", markershape=:auto)
+	plot!(p, times, exp_mean_2, label = "Exp(2)", markershape=:auto)
+	plot!(p, times, exp_mean_05, label = "Exp(1/2)", markershape=:auto)
+	plot!(
+		p, times, heavy_tailed_mean, 
+		label = "$(L"F(x)=\left(1-\frac{1}{x^2}\right)\mathbb{1}_{x>1}")",
+		markershape=:auto
+	)
+	p
+end
 
 # ╔═╡ Cell order:
 # ╠═98aec1c2-21e1-11eb-31dd-a19c407f373c
 # ╠═dca3e9a0-21e1-11eb-22b4-07c891533980
+# ╠═32c4fca2-2392-11eb-133e-213db77f04d1
+# ╠═bcc952c0-2392-11eb-1e54-0b4d8fa5c73b
+# ╠═d1d3f3c0-2395-11eb-00ea-59aa6de8c60f
--- OrderStatistics/src/OrderStatistics.jl
+++ OrderStatistics/src/OrderStatistics.jl
@@ -6,16 +6,16 @@ import CUDA: @cuda
 
 include("rngSquares.jl")
 
-function gpu_parallel!(results, sampleSize, pseudoInverse, seed::UInt64, callable, rng::RNG)
+function gpu_parallel!(results, sampleSize, pseudoInverse, seed::UInt64, callable)
     index = (CUDA.blockIdx().x - 1) * CUDA.blockDim().x + CUDA.threadIdx().x
     stride = CUDA.blockDim().x * CUDA.gridDim().x
     for thread in index:stride:length(results)
-        rvg = rng(pseudoInverse, UInt64(thread), UInt64(length(results)), UInt64(sampleSize), seed)
+        rvg = unifWithPseudoInvGen(pseudoInverse, UInt64(thread), UInt64(length(results)), UInt64(sampleSize), seed)
         results[thread] = callable(rvg)
     end
 end
 
-function sample_extreme_values(sampleSize, superSampleSize, pseudoInverse, callable=largest, seed=get_seed(), rng::RNG=unifWithPseudoInvGen)::Array{Float32,1}
+function sample_extreme_values(sampleSize, superSampleSize, pseudoInverse, callable=largest, seed=get_seed())::Array{Float32,1}
     if typeof(pseudoInverse(0.5)) <: Tuple
         # monte carlo pseudoInverse (can fail -> Tuple{rv, accepted::Bool})
         fixedPseudoInverse = pseudoInverse
@@ -26,7 +26,7 @@ function sample_extreme_values(sampleSize, superSampleSize, pseudoInverse, calla
     numblocks = ceil(Int, superSampleSize/256)
     gpu_res = CUDA.CuArray{Float32}(undef, superSampleSize)
     cpu_res = Array{Float32}(undef, superSampleSize)
-    @cuda threads=256 blocks=numblocks gpu_parallel!(gpu_res, sampleSize, fixedPseudoInverse, seed, callable, rng)
+    @cuda threads=256 blocks=numblocks gpu_parallel!(gpu_res, sampleSize, fixedPseudoInverse, seed, callable)
     copyto!(cpu_res, gpu_res)
     return cpu_res
 end
